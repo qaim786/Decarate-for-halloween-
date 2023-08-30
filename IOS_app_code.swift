a483de7b1057fb787466b5a8b42567d93a731842
Qaim <104954885+qaim786@users.noreply.github.com>
2023-08-30 12:58:03 -0400

    Add files via upload

---
+++ IOS_app_code.swift
@@ -0,0 +1,398 @@
+// iPhone 13 / 13 Pro - 1
+
+
+var view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 5
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 6
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 4
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 7
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 8
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 9
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 10
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 11
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 12
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 13
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 20
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 21
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 14
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 15
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 22
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 18
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// iPhone 13 / 13 Pro - 17
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+
+view.backgroundColor = .white
+
+
+view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+
+
+
+
+// image 36
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+view.backgroundColor = .white
+
+
+let image0 = UIImage(named: ".png")?.cgImage
+
+let layer0 = CALayer()
+
+layer0.contents = image0
+
+layer0.bounds = view.bounds
+
+layer0.position = view.center
+
+view.layer.addSublayer(layer0)
+
+
+
+
+
+// image 35
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+view.backgroundColor = .white
+
+
+let image0 = UIImage(named: ".png")?.cgImage
+
+let layer0 = CALayer()
+
+layer0.contents = image0
+
+layer0.bounds = view.bounds
+
+layer0.position = view.center
+
+view.layer.addSublayer(layer0)
+
+
+
+
+
+// image 34
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+view.backgroundColor = .white
+
+
+let image0 = UIImage(named: ".png")?.cgImage
+
+let layer0 = CALayer()
+
+layer0.contents = image0
+
+layer0.bounds = view.bounds
+
+layer0.position = view.center
+
+view.layer.addSublayer(layer0)
+
+
+
+
+
+// image 33
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+view.backgroundColor = .white
+
+
+let image0 = UIImage(named: ".png")?.cgImage
+
+let layer0 = CALayer()
+
+layer0.contents = image0
+
+layer0.bounds = view.bounds
+
+layer0.position = view.center
+
+view.layer.addSublayer(layer0)
+
+
+
+
+
+// image 32
+
+
+view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+view.backgroundColor = .white
+
+
+let image0 = UIImage(named: ".png")?.cgImage
+
+let layer0 = CALayer()
+
+layer0.contents = image0
+
+layer0.bounds = view.bounds
+
+layer0.position = view.center
+
+view.layer.addSublayer(layer0)
+
+
+
